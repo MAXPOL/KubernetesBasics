927234444e8135b411ddbc81dd313634177f9c6b
MAXPOL <maxrol32@gmail.com>
2020-12-22 23:37:17 +0300

    Update kuberneteBasic.sh

--- kuberneteBasic.sh
+++ kuberneteBasic.sh
@@ -3,7 +3,7 @@ echo "192.168.0.4 wsone.local" > /etc/hosts # Change IP address on ip addres you
 echo "192.168.0.5 master.local" > /etc/hosts # Change IP address on ip addres your master 
 echo "192.168.0.6 wstwo.local" > /etc/hosts # Change IP address on ip addres your master 
 swapoff -a
-kubeadm init --apiserver-advertise-address=192.168.0.5 --pod-network-cidr=10.244.0.0/16 >> /kubeadminstallinfo.data# Change IP address on ip addres your master 
+kubeadm init --apiserver-advertise-address=192.168.0.5 --pod-network-cidr=10.244.0.0/16 >> /kubeadminstallinfo.data # Change IP address on ip addres your master 
 echo "user    ALL=(ALL)       ALL" > /etc/sudoers
 su user
 mkdir -p $HOME/.kube
