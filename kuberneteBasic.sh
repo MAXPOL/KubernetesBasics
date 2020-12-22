3b8ea74159cc6f41621146e734f9ad1535bc357b
MAXPOL <maxrol32@gmail.com>
2020-12-22 21:12:50 +0300

    add string swapoff -a

--- kuberneteBasic.sh
+++ kuberneteBasic.sh
@@ -2,6 +2,7 @@
 echo "192.168.0.4 wsone.local" > /etc/hosts # Change IP address on ip addres your master 
 echo "192.168.0.5 master.local" > /etc/hosts # Change IP address on ip addres your master 
 echo "192.168.0.6 wstwo.local" > /etc/hosts # Change IP address on ip addres your master 
+swapoff -a
 kubeadm init --apiserver-advertise-address=192.168.0.6 --pod-network-cidr=10.244.0.0/16 # Change IP address on ip addres your master 
 echo "user    ALL=(ALL)       ALL" > /etc/sudoers
 su user
