9b10d147c9caffd79c09ceee9943998cf6571a61
MAXPOL <maxrol32@gmail.com>
2020-12-22 20:09:15 +0300

    Add firewalld ports

--- kubernetesInit.sh
+++ kubernetesInit.sh
@@ -6,4 +6,5 @@ cp k8s.repo /etc/yum.repos.d/
 yum install kubeadm kubectl kubelet -y
 systemctl enable kubelet
 sed -i "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
+firewall-cmd --permanent --zone=public --add-port={6443,2379,2380,10250,10251,10252,10255}/tcp
 reboot
