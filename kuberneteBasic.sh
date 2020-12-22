626a23e3ca71cb3fb84b6f51478cf2f22c678e5f
MAXPOL <maxrol32@gmail.com>
2020-12-22 20:38:40 +0300

    Update kuberneteBasic.sh

--- kuberneteBasic.sh
+++ kuberneteBasic.sh
@@ -1,4 +1,11 @@
-
-
-
+#!/bin/bash
+echo "192.168.0.4 wsone.local" > /etc/hosts # Change IP address on ip addres your master 
+echo "192.168.0.5 master.local" > /etc/hosts # Change IP address on ip addres your master 
+echo "192.168.0.6 wstwo.local" > /etc/hosts # Change IP address on ip addres your master 
+kubeadm init --apiserver-advertise-address=192.168.0.6 --pod-network-cidr=10.244.0.0/16 # Change IP address on ip addres your master 
 echo "user    ALL=(ALL)       ALL" > /etc/sudoers
+su user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
