8cdebd69ad204edccfb450503c5a3011c0240788
MAXPOL <maxrol32@gmail.com>
2020-12-22 21:16:22 +0300

    Update kuberneteBasic.sh

--- kuberneteBasic.sh
+++ kuberneteBasic.sh
@@ -9,4 +9,4 @@ su user
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
